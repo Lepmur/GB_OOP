b05fb8affba45cfcb07401e6a3a4454d1aa5116b
Sergei <ip744692@yandex.ru>
2023-06-10 14:51:37 +0300

    added Sem_5 Lec_5 DZ_5 NO COMPLETE!

---
+++ DZ_5_100623/src/main/java/ru/geekbrains/presenters/View.java
@@ -0,0 +1,24 @@
+package ru.geekbrains.presenters;
+
+import ru.geekbrains.models.Table;
+
+import java.util.Collection;
+
+public interface View {
+
+    /**
+     * Отобразить список всех столиков
+     * @param tables список столиков
+     */
+    void showTables(Collection<Table> tables);
+
+    void showReservationStatus(int reservationNo);
+
+    /**
+     * Установить нового наблюдателя
+     * @param observer наблюдатель (презентер)
+     */
+    void setObserver(ViewObserver observer);
+
+
+}
