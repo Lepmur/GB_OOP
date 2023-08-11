1132b7476c0ec9ca79372c54ff03d4dfaaf9fe6a
Sergei <ip744692@yandex.ru>
2023-08-11 09:39:26 +0300

    ALG_DZ_1_110823

---
+++ Alg_DZ_1_110823/src/main/java/org/example/HeapSort.java
@@ -0,0 +1,65 @@
+package org.example;
+
+public class HeapSort {
+    public static void heapSort(int[] arr) {
+        int n = arr.length;
+
+        // Построение кучи (перегруппировка массива)
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        // Извлечение элементов из кучи один за другим
+        for (int i = n - 1; i >= 0; i--) {
+            // Перемещаем текущий корень в конец массива
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // Вызываем heapify на уменьшенной куче
+            heapify(arr, i, 0);
+        }
+    }
+
+    private static void heapify(int[] arr, int n, int i) {
+        int largest = i; // Инициализируем наибольший элемент как корень
+        int left = 2 * i + 1; // Левый дочерний элемент
+        int right = 2 * i + 2; // Правый дочерний элемент
+
+        // Если левый дочерний элемент больше корня
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        // Если правый дочерний элемент больше, чем самый большой элемент на данный момент
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        // Если самый большой элемент не корень
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Рекурсивно вызываем heapify для поддерева
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6, 7};
+
+        System.out.println("Исходный массив:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        heapSort(arr);
+
+        System.out.println("\nОтсортированный массив:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
